3b3ee190d954b294d57afd54540ebb2f0bc236f9
Suhoniak <vladyslav.suhoniak.c@greenwayhealth.com>
2024-01-16 10:39:58 +0200

    minor fixes and refactoring

--- src/main/java/com/realo/adminservice/controllers/UserController.java
+++ src/main/java/com/realo/adminservice/controllers/UserController.java
@@ -10,7 +10,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping("/admin-service/user")
+@RequestMapping("/admin/user")
 public class UserController {
     private final UserService userService;
 
